0dd3fa41fe8243e2937faf253f8c1ed2483e53bb
Omar MEBARKI <omar.mebarki@gmail.com>
2019-12-23 15:21:52 +0100

    Init Project

---
+++ src/main/java/omar/mebarki/elfinder/config/Index.java
@@ -0,0 +1,12 @@
+package omar.mebarki.elfinder.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+    @RequestMapping("/")
+    String index() {
+        return "elfinder";
+    }
+}
